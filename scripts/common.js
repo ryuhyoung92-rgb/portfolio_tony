4f4dcbae042e240fa8cb48623f0b610f7141cb00
ryuhyoung92-rgb <ryuhyoung92@gmail.com>
2026-04-17 17:48:36 +0900

    hero banner 작업 중

--- scripts/common.js
+++ scripts/common.js
@@ -1,1 +1,35 @@
 // common.js
+const productSwiper = document.querySelectorAll('.container .list_container .swiper');
+
+console.log(productSwiper);
+
+/* const bestSwiper = new Swiper(productSwiper[0],{
+    slidesPerView:'auto',
+    spaceBetween:30,
+    slidesPerGroup:4,
+    pagination:{
+        el:'.list_container .swiper .swiper-pagination',
+    }
+}) */
+
+// index 제품 스와이퍼 JS
+function initSwiper(target){
+    return new Swiper(target, {
+        slidesPerView:'auto',
+        spaceBetween:30,
+        slidesPerGroup:4,
+        pagination:{
+            el:'.list_container .swiper .swiper-pagination',
+        },
+    // swiper navigation
+        navigation:{
+            prevEl:'.swiper .control_bar .prev',
+            nextEl:'.swiper .control_bar .next',
+        },
+    }
+    )};
+
+initSwiper(productSwiper[0]);
+initSwiper(productSwiper[1]);
+initSwiper(productSwiper[2]);
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,22 @@
 // index.js
-const productSwiper = document.querySelectorAll('.container .list_container .swiper');
+// here_bnr swiper
+const heroBnrSwiper = document.querySelector('#hero_bnr .swiper');
 
-console.log(productSwiper);
+console.log(heroBnrSwiper);
 
-/* const bestSwiper = new Swiper(productSwiper[0],{
-    slidesPerView:'auto',
-    spaceBetween:30,
-    slidesPerGroup:4,
+const mainSwiper = new Swiper(heroBnrSwiper, {
+    autoplay:{delay:4000,},
+    loop:true,
+    
     pagination:{
-        el:'.list_container .swiper .swiper-pagination',
-    }
-}) */
-
-// index 제품 스와이퍼 JS
-function initSwiper(target){
-    return new Swiper(target, {
-        slidesPerView:'auto',
-        spaceBetween:30,
-        slidesPerGroup:4,
-        pagination:{
-            el:'.list_container .swiper .swiper-pagination',
-        },
-    // swiper navigation
-        navigation:{
-            prevEl:'.swiper .control_bar .prev',
-            nextEl:'.swiper .control_bar .next',
-        },
-    }
-    )};
-
-initSwiper(productSwiper[0]);
-initSwiper(productSwiper[1]);
-initSwiper(productSwiper[2]);
+        el:'#hero_bnr .control_bar .swiper-pagination',
+        type:'fraction',
+    },
+    navigation:{
+        prevEl:'#hero_bnr .control_bar .prev',
+        nextEl:'#hero_bnr .control_bar .next',
+    },
+})
 
 
 // best seller 카테고리 이벤트 JS
